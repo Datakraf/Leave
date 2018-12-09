efae6dc121fd699f3fd88464d29f38b7b27b3fa8
Uzzair Baharudin <uzzaircode@gmail.com>
2018-12-10 06:55:09 +0800

    add actions

--- Resources/views/leave/admin/leave-records.blade.php
+++ Resources/views/leave/admin/leave-records.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h3 class="cad-title">Employees List</h3>
+        <h3 class="cad-title">Employees Leave Records</h3>
     </div>
     <div class="card-body">
-        @include('components.table.table',['columnHeaders'=>$userColumnHeaders,'results'=>$userResults,'rows'=>$userRows,'actions'=>$actions])
+        @include('components.table.table',['columnHeaders'=>$userColumnHeaders,'results'=>$userResults,'rows'=>$userRows,'actions'=>$userActions])
     </div>
 </div>
 <!-- user records -->
